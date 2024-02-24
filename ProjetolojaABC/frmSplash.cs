0554a88ea76ce905bfabff081b78151ebcbc43f3
RafaelGMC <rafael.g.de.m.carvalho@hotmail.com>
2024-02-23 21:59:31 -0300

    splash finalizada

--- ProjetolojaABC/frmSplash.cs
+++ ProjetolojaABC/frmSplash.cs
@@ -16,5 +16,10 @@ namespace ProjetolojaABC
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
